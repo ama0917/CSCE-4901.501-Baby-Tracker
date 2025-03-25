eb4fe5ed48f3b5def46cccd4e996e648a75a1598
cen0075 <144827164+cen0075@users.noreply.github.com>
2025-03-25 18:55:04 -0500

    Adding initial database files

---
+++ childDatabase.py
@@ -0,0 +1,4 @@
+import sqlite3 as sqlite3
+import json
+
+# child Database will hold name, date of birth, etc.) and their sleep (start/end time), feeding (type, amount, time), and diaper records (time, type)
